ac7cdf7aff3aa3a60c95d4b91abc9f6e88ee5092
alinabzykova <bzykovaaa@gmail.com>
2026-04-04 18:14:36 +0300

    Установила tailwind

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="bg-red-500 text-white p-10">
+    Проверка Tailwind
+  </div>
 </template>
 
 <style scoped>
